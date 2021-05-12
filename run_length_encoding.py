3ba8db64d4a7df050fa52b6251208b1bb67c4651
aprilycliu <34789640+aprilycliu@users.noreply.github.com>
2021-05-12 10:17:38 +0300

    Create run_length_encoding.py

---
+++ run_length_encoding.py
@@ -0,0 +1,46 @@
+def run_length_encode(data):
+    """
+    Apply RLE on an array of data, return [values, repetitions].
+    """
+    values = []
+    repetitions = []
+    last_item = data[0]
+    item_count = 1
+    for i in range(1, len(data)):
+        d = data[i]
+        if last_item != d:
+            values.append(last_item)
+            repetitions.append(item_count)
+            last_item = d
+            item_count = 1
+        else:
+            item_count += 1
+    values.append(last_item)
+    repetitions.append(item_count)
+
+    return values, repetitions
+
+
+def run_length_decode(values, repetitions):
+    original_data = []
+    for value, repetition in zip(values, repetitions):
+        for _ in range(repetition):
+            original_data.append(value)
+    return original_data
+  
+  
+  original_bytes = np.product(grayscale_image.shape)
+
+from skimage.io import imread  
+original_image = imread("building.png")
+grayscale_image = original_image[:, :, 0]
+
+original_bytes = np.product(grayscale_image.shape)
+
+flat_grayscale_image = grayscale_image.ravel().tolist()
+grayscale_values, grayscale_repetitions = run_length_encode(flat_grayscale_image)
+reconstructed_grayscale_image = run_length_decode(grayscale_values, grayscale_repetitions)
+reconstructed_grayscale_image = np.array(reconstructed_grayscale_image).reshape(grayscale_image.shape)
+
+# Both values and repetitions are stored as 8-bit integers
+compression = (len(grayscale_values) + (len(grayscale_repetitions))) / original_bytes
